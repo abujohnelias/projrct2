e2919f3f0caec9b3aa5a4d6ae4b17ba21a321766
NivedyaN <nivedyapattari12@gmail.com>
2022-11-21 15:32:50 +0530

    commit

---
+++ bin/class.dart
@@ -0,0 +1,10 @@
+class classresume {
+  //instance variable =>globally declered variables/inside the class outside other statement
+  String name = "ABU";
+  int age = 21;
+  int phone = 9876543210;
+  double mark = 9.9;
+  String email = "abu10101010@gmail.com";
+
+}
+void main ()
