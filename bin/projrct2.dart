014468061a7e3f9ca919171fbde5478fff034fc2
NivedyaN <nivedyapattari12@gmail.com>
2022-11-21 15:12:02 +0530

    Initial commit

---
+++ bin/projrct2.dart
@@ -0,0 +1,14 @@
+void main(){
+  String name = "ABU";
+  int age = 21;
+  int phone = 9876543210;
+  double mark = 9.9;
+  String email = "abu10101010@gmail.com";
+
+  print("..my resume..");
+  print("my name is $name");
+  print ( "iam $age yrs old");
+  print("my phone number is $phone");
+  print("i've $mark in graduation");
+  print("my email id is $email");
+}
---
+++ test/projrct2_test.dart
@@ -0,0 +1,8 @@
+import 'package:projrct2/projrct2.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
